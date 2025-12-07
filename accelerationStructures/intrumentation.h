5283ae6728caf88df487bb56111889d5e4c55926
mmmuscus <mmmuscus@gmail.com>
2025-12-07 20:30:24 +0100

    instrumentation class

---
+++ accelerationStructures/intrumentation.h
@@ -0,0 +1,41 @@
+#ifndef INSTRUMENTATION_H_
+#define INSTRUMENTATION_H_
+
+#include "../general/general.h"
+
+class instrumentation {
+private:
+	unsigned int intersectionTests;
+	unsigned int traversalSteps;
+
+public:
+	instrumentation() : intersectionTests(0), traversalSteps(0) {}
+
+	void increaseIntersection() {
+		intersectionTests++;
+	}
+
+	void increaseTraversal() {
+		traversalSteps++;
+	}
+
+	void reset() {
+		intersectionTests = 0;
+		traversalSteps = 0;
+	}
+
+	void printIntersection() {
+		std::cout << "Number of intersection tests: " << intersectionTests << std::endl;
+	}
+
+	void printTraversal() {
+		std::cout << "Number of traversal steps: " << traversalSteps << std::endl;
+	}
+
+	void print() {
+		printIntersection();
+		printTraversal();
+	}
+};
+
+#endif // !INSTRUMENTATION_H_
