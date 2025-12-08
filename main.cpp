aa02607b56c0dd2f6a92976bec9d281472514e60
mmmuscus <mmmuscus@gmail.com>
2025-12-08 01:06:40 +0100

    mesh loaded badly

--- main.cpp
+++ main.cpp
@@ -38,8 +38,53 @@ int main()
     openGlW.createScreenTexture();
     std::cout << "OpenGL Context successfully created" << std::endl;
 
+    // Guided by: https://vulkan-tutorial.com/Loading_models
+    // Also by: https://github.com/canmom/rasteriser/blob/master/fileloader.cpp
+    // Load model
+    std::cout << "Loading model started" << std::endl;
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string warn, err;
+    std::string path = "assets/teapot.obj";
+
+    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, path.c_str())) {
+        throw std::runtime_error(err);
+    }
+
+    std::vector<glm::vec3> vertices;
+
+    // Load vertices
+    for (const auto& shape : shapes) {
+        const std::vector<tinyobj::index_t>& indices = shape.mesh.indices;
+        const std::vector<int>& mat_ids = shape.mesh.material_ids;
+
+        for (size_t face_ind = 0; face_ind < mat_ids.size(); face_ind++) {
+            triangle prim;
+
+            prim.p0 = glm::vec3(
+                attrib.vertices[3 * indices[3 * face_ind].vertex_index + 0],
+                attrib.vertices[3 * indices[3 * face_ind].vertex_index + 1],
+                attrib.vertices[3 * indices[3 * face_ind].vertex_index + 2]
+            );
+            prim.p1 = glm::vec3(
+                attrib.vertices[3 * indices[3 * face_ind + 1].vertex_index + 0],
+                attrib.vertices[3 * indices[3 * face_ind + 1].vertex_index + 1],
+                attrib.vertices[3 * indices[3 * face_ind + 1].vertex_index + 2]
+            );
+            prim.p2 = glm::vec3(
+                attrib.vertices[3 * indices[3 * face_ind + 2].vertex_index + 0],
+                attrib.vertices[3 * indices[3 * face_ind + 2].vertex_index + 1],
+                attrib.vertices[3 * indices[3 * face_ind + 2].vertex_index + 2]
+            );
+            prim.calculateCentroid();
+
+            prims.push_back(prim);
+        }
+    }
+
     // Scene
-    sc.initScene();
+    //sc.initScene();
     std::cout << "Scene successfully populated" << std::endl;
 
     // Build BVH
