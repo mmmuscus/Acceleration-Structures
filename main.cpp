b5626b1107efd1b63d92fbdc40d9d8884ab0549b
mmmuscus <mmmuscus@gmail.com>
2026-01-21 16:36:19 +0100

    Every scene, every render

--- main.cpp
+++ main.cpp
@@ -49,6 +49,8 @@ int main()
     base_RDH_10_12.deserialize("BVHs/base_RDH-10-12.txt");
     BVH base_RDH_20_20;
     base_RDH_20_20.deserialize("BVHs/base_RDH-20-20.txt");
+    BVH base_RDHSAHBlend_6_2;
+    base_RDHSAHBlend_6_2.deserialize("BVHs/base_RDHSAHBlend-6-2.txt");
     BVH base_RDHSAHBlend_6_4;
     base_RDHSAHBlend_6_4.deserialize("BVHs/base_RDHSAHBlend-6-4.txt");
     BVH base_RDHSAHBlend_6_10;
@@ -76,6 +78,8 @@ int main()
     LOA_RDH_10_12.deserialize("BVHs/LOA_RDH-10-12.txt");
     BVH LOA_RDH_20_20;
     LOA_RDH_20_20.deserialize("BVHs/LOA_RDH-20-20.txt");
+    BVH LOA_RDHSAHBlend_6_2;
+    LOA_RDHSAHBlend_6_2.deserialize("BVHs/LOA_RDHSAHBlend-6-2.txt");
     BVH LOA_RDHSAHBlend_6_4;
     LOA_RDHSAHBlend_6_4.deserialize("BVHs/LOA_RDHSAHBlend-6-4.txt");
     BVH LOA_RDHSAHBlend_6_10;
@@ -103,6 +107,8 @@ int main()
     HOA_RDH_10_12.deserialize("BVHs/HOA_RDH-10-12.txt");
     BVH HOA_RDH_20_20;
     HOA_RDH_20_20.deserialize("BVHs/HOA_RDH-20-20.txt");
+    BVH HOA_RDHSAHBlend_6_2;
+    HOA_RDHSAHBlend_6_2.deserialize("BVHs/HOA_RDHSAHBlend-6-2.txt");
     BVH HOA_RDHSAHBlend_6_4;
     HOA_RDHSAHBlend_6_4.deserialize("BVHs/HOA_RDHSAHBlend-6-4.txt");
     BVH HOA_RDHSAHBlend_6_10;
@@ -118,78 +124,6 @@ int main()
     BVH HOA_OH_20;
     HOA_OH_20.deserialize("BVHs/HOA_OH-20.txt");
 
-    //// naive and SAH for all scenes
-    //sc.setSceneGeometry(scGeos.base);
-    //naiveBvh = BVH(NAIVE);
-    //SAHBvh = BVH(SAH);
-    //naiveBvh.buildAndSerialize("BVHs/base_naive.txt");
-    //SAHBvh.buildAndSerialize("BVHs/base_SAH.txt");
-    //sc.setSceneGeometry(scGeos.lowOcclusionA);
-    //naiveBvh = BVH(NAIVE);
-    //SAHBvh = BVH(SAH);
-    //naiveBvh.buildAndSerialize("BVHs/LOA_naive.txt");
-    //SAHBvh.buildAndSerialize("BVHs/LOA_SAH.txt");
-    ///*sc.setSceneGeometry(scGeos.lowOcclusionB);
-    //naiveBvh = BVH(NAIVE);
-    //SAHBvh = BVH(SAH);
-    //naiveBvh.buildAndSerialize("BVHs/LOB_naive.txt");
-    //SAHBvh.buildAndSerialize("BVHs/LOB_SAH.txt");*/
-    //sc.setSceneGeometry(scGeos.highOcclusionA);
-    //naiveBvh = BVH(NAIVE);
-    //SAHBvh = BVH(SAH);
-    //naiveBvh.buildAndSerialize("BVHs/HOA_naive.txt");
-    //SAHBvh.buildAndSerialize("BVHs/HOA_SAH.txt");
-    ///*sc.setSceneGeometry(scGeos.highOcclusionB);
-    //naiveBvh = BVH(NAIVE);
-    //SAHBvh = BVH(SAH);
-    //naiveBvh.buildAndSerialize("BVHs/HOB_naive.txt");
-    //SAHBvh.buildAndSerialize("BVHs/HOB_SAH.txt");*/
-    //
-    //// OHs for important scenes
-    //sc.setSceneGeometry(scGeos.base);
-    //OHBvh = BVH(OH);
-    //OHBvh.buildAndSerialize("BVHs/base_OH-6.txt", sc.getCam(), 6);
-    //OHBvh = BVH(OH);
-    //OHBvh.buildAndSerialize("BVHs/base_OH-10.txt", sc.getCam(), 10);
-    //OHBvh = BVH(OH);
-    //OHBvh.buildAndSerialize("BVHs/base_OH-20.txt", sc.getCam(), 20);
-    //sc.setSceneGeometry(scGeos.lowOcclusionA);
-    //OHBvh = BVH(OH);
-    //OHBvh.buildAndSerialize("BVHs/LOA_OH-6.txt", sc.getCam(), 6);
-    //OHBvh = BVH(OH);
-    //OHBvh.buildAndSerialize("BVHs/LOA_OH-10.txt", sc.getCam(), 10);
-    //OHBvh = BVH(OH);
-    //OHBvh.buildAndSerialize("BVHs/LOA_OH-20.txt", sc.getCam(), 20);
-    //sc.setSceneGeometry(scGeos.highOcclusionA);
-    //OHBvh = BVH(OH);
-    //OHBvh.buildAndSerialize("BVHs/HOA_OH-6.txt", sc.getCam(), 6);
-    //OHBvh = BVH(OH);
-    //OHBvh.buildAndSerialize("BVHs/HOA_OH-10.txt", sc.getCam(), 10);
-    //OHBvh = BVH(OH);
-    //OHBvh.buildAndSerialize("BVHs/HOA_OH-20.txt", sc.getCam(), 20);
-
-    // Important RDHs for important scenes
-    /*sc.setSceneGeometry(scGeos.base);
-    RDHSAHBlendBvh = BVH(RDHSAHBLEND);
-    RDHSAHBlendBvh.buildAndSerialize("BVHs/base_RDHSAHBlend-6-2.txt", sc.getCam(), 6, 2);
-    sc.setSceneGeometry(scGeos.lowOcclusionA);
-    RDHSAHBlendBvh = BVH(RDHSAHBLEND);
-    RDHSAHBlendBvh.buildAndSerialize("BVHs/LOA_RDHSAHBlend-6-2.txt", sc.getCam(), 6, 2);
-    sc.setSceneGeometry(scGeos.highOcclusionA);
-    RDHSAHBlendBvh = BVH(RDHSAHBLEND);
-    RDHSAHBlendBvh.buildAndSerialize("BVHs/HOA_RDHSAHBlend-6-2.txt", sc.getCam(), 6, 2);*/
-
-    // Deserialiaze BVHs
-    //naiveBvh.deserialize("BVHs/naive.txt");
-    //SAHBvh.deserialize("BVHs/SAH.txt");
-    //BVH RDHBvh = BVH(RDH);
-    //RDHBvh.deserialize("BVHs/base_RDH-6-5.txt");
-    //RDHSAHBlendBvh.deserialize("BVHs/RDHSAHBlended-3-8.txt");
-    //OHBvh.deserialize("BVHs/OH-3");
-
-    // Init scene
-    sc.setSceneGeometry(scGeos.base);
-    std::cout << "Scene successfully populated" << std::endl;
 
     // Create windows, and API contexts
     glfwW.createGLFWContext();
@@ -214,6 +148,105 @@ int main()
     openGlW.createScreenTexture();
     std::cout << "OpenGL Context successfully created" << std::endl;
 
+    // BVH render spinning
+    openGlW.setTextureIndex(3);
+
+    // base scene
+    // Init scene
+    sc.setSceneGeometry(scGeos.base);
+    std::cout << std::endl << "!!! base Scene loaded" << std::endl;
+    std::cout << "Evaulating base_naive" << std::endl;
+    spinBVHEval(base_naive);
+    std::cout << "Evaulating base_SAH" << std::endl;
+    spinBVHEval(base_SAH);
+    std::cout << "Evaulating base_RDH_6_4" << std::endl;
+    spinBVHEval(base_RDH_6_4);
+    std::cout << "Evaulating base_RDH_6_5" << std::endl;
+    spinBVHEval(base_RDH_6_5);
+    std::cout << "Evaulating base_RDH_6_10" << std::endl;
+    spinBVHEval(base_RDH_6_10);
+    std::cout << "Evaulating base_RDH_10_12" << std::endl;
+    spinBVHEval(base_RDH_10_12);
+    std::cout << "Evaulating base_RDH_20_20" << std::endl;
+    spinBVHEval(base_RDH_20_20);
+    std::cout << "Evaulating base_RDHSAHBlend_6_2" << std::endl;
+    spinBVHEval(base_RDHSAHBlend_6_2);
+    std::cout << "Evaulating base_RDHSAHBlend_6_4" << std::endl;
+    spinBVHEval(base_RDHSAHBlend_6_4);
+    std::cout << "Evaulating base_RDHSAHBlend_6_10" << std::endl;
+    spinBVHEval(base_RDHSAHBlend_6_10);
+    std::cout << "Evaulating base_RDHSAHBlend_10_12" << std::endl;
+    spinBVHEval(base_RDHSAHBlend_10_12);
+    std::cout << "Evaulating base_RDHSAHBlend_20_20" << std::endl;
+    spinBVHEval(base_RDHSAHBlend_20_20);
+    std::cout << "Evaulating base_OH_6" << std::endl;
+    spinBVHEval(base_OH_6);
+    std::cout << "Evaulating base_OH_10" << std::endl;
+    spinBVHEval(base_OH_10);
+    std::cout << "Evaulating base_OH_20" << std::endl;
+    spinBVHEval(base_OH_20);
+
+    sc.setSceneGeometry(scGeos.lowOcclusionA);
+    std::cout << std::endl << "!!! LOA Scene loaded" << std::endl;
+    std::cout << "Evaulating LOA_naive" << std::endl;
+    spinBVHEval(LOA_naive);
+    std::cout << "Evaulating LOA_SAH" << std::endl;
+    spinBVHEval(LOA_SAH);
+    std::cout << "Evaulating LOA_RDH_6_4" << std::endl;
+    spinBVHEval(LOA_RDH_6_4);
+    std::cout << "Evaulating LOA_RDH_6_10" << std::endl;
+    spinBVHEval(LOA_RDH_6_10);
+    std::cout << "Evaulating LOA_RDH_10_12" << std::endl;
+    spinBVHEval(LOA_RDH_10_12);
+    std::cout << "Evaulating LOA_RDH_20_20" << std::endl;
+    spinBVHEval(LOA_RDH_20_20);
+    std::cout << "Evaulating LOA_RDHSAHBlend_6_2" << std::endl;
+    spinBVHEval(LOA_RDHSAHBlend_6_2);
+    std::cout << "Evaulating LOA_RDHSAHBlend_6_4" << std::endl;
+    spinBVHEval(LOA_RDHSAHBlend_6_4);
+    std::cout << "Evaulating LOA_RDHSAHBlend_6_10" << std::endl;
+    spinBVHEval(LOA_RDHSAHBlend_6_10);
+    std::cout << "Evaulating LOA_RDHSAHBlend_10_12" << std::endl;
+    spinBVHEval(LOA_RDHSAHBlend_10_12);
+    std::cout << "Evaulating LOA_RDHSAHBlend_20_20" << std::endl;
+    spinBVHEval(LOA_RDHSAHBlend_20_20);
+    std::cout << "Evaulating LOA_OH_6" << std::endl;
+    spinBVHEval(LOA_OH_6);
+    std::cout << "Evaulating LOA_OH_10" << std::endl;
+    spinBVHEval(LOA_OH_10);
+    std::cout << "Evaulating LOA_OH_20" << std::endl;
+    spinBVHEval(LOA_OH_20);
+
+    sc.setSceneGeometry(scGeos.highOcclusionA);
+    std::cout << std::endl << "!!! HOA Scene loaded" << std::endl;
+    std::cout << "Evaulating HOA_naive" << std::endl;
+    spinBVHEval(HOA_naive);
+    std::cout << "Evaulating HOA_SAH" << std::endl;
+    spinBVHEval(HOA_SAH);
+    std::cout << "Evaulating HOA_RDH_6_4" << std::endl;
+    spinBVHEval(HOA_RDH_6_4);
+    std::cout << "Evaulating HOA_RDH_6_10" << std::endl;
+    spinBVHEval(HOA_RDH_6_10);
+    std::cout << "Evaulating HOA_RDH_10_12" << std::endl;
+    spinBVHEval(HOA_RDH_10_12);
+    std::cout << "Evaulating HOA_RDH_20_20" << std::endl;
+    spinBVHEval(HOA_RDH_20_20);
+    std::cout << "Evaulating HOA_RDHSAHBlend_6_2" << std::endl;
+    spinBVHEval(HOA_RDHSAHBlend_6_2);
+    std::cout << "Evaulating HOA_RDHSAHBlend_6_4" << std::endl;
+    spinBVHEval(HOA_RDHSAHBlend_6_4);
+    std::cout << "Evaulating HOA_RDHSAHBlend_6_10" << std::endl;
+    spinBVHEval(HOA_RDHSAHBlend_6_10);
+    std::cout << "Evaulating HOA_RDHSAHBlend_10_12" << std::endl;
+    spinBVHEval(HOA_RDHSAHBlend_10_12);
+    std::cout << "Evaulating HOA_RDHSAHBlend_20_20" << std::endl;
+    spinBVHEval(HOA_RDHSAHBlend_20_20);
+    std::cout << "Evaulating HOA_OH_6" << std::endl;
+    spinBVHEval(HOA_OH_6);
+    std::cout << "Evaulating HOA_OH_10" << std::endl;
+    spinBVHEval(HOA_OH_10);
+    std::cout << "Evaulating HOA_OH_20" << std::endl;
+    spinBVHEval(HOA_OH_20);
 
     // BVH render static
     /*
@@ -225,13 +258,6 @@ int main()
     openGlW.render();
     glfwW.swapBuffers();
     */
-
-    // BVH render spinning
-    openGlW.setTextureIndex(3);
-
-    st
-    spinBVHEval(base_OH_20);
-    spinBVHEval(base_OH_20);
 
     // Main loop
     while (!glfwWindowShouldClose(glfwW.getWindow()))
